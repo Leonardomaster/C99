175386d60dcdcc385779be2e36acc2ae6c7bed6e
Leonardomaster <123128747+Leonardomaster@users.noreply.github.com>
2023-05-25 21:14:03 -0300

    Add files via upload

---
+++ game_login (2).js
@@ -0,0 +1,65 @@
+// Crie a função addUser()
+function addUser {
+  // Obtenha o valor do usuário através das ids player1_name_input e player2_name_input
+  player1_name_input
+   player2_name_input
+  // Armazene esses valores localmente
+  window.location(game_page.html)
+  // Atribua "game_page.html" para window.location
+}
+
+function send() {
+   number1 = document.getElementById("number1").value;
+   number2 = document.getElementById("number2").value;
+   actual_answer = parseInt(number1) * parseInt(number2);
+   question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+   input_box = "<br>Resposta : <input type='text' id='input_check_box'>";
+   check_button = "<br><br><button class='btn btn-info' onclick='check()'>Verificar</button>";
+   row = question_number + input_box + check_button ; 
+   document.getElementById("output").innerHTML = row
+   document.getElementById("number1").value = "";
+   document.getElementById("number2").value = "";
+}
+
+question_turn = "player1"
+answer_turn = "player2"
+
+function check()
+{
+  get_answer = document.getElementById("input_check_box").value;
+  if(get_answer == actual_answer)
+  {
+    if(answer_turn == "player1")
+    {
+      player1_score = player1_score +1;
+      document.getElementById("player1_score").innerHTML = player1_score
+    }
+    else
+    {
+      player2_score = player2_score +1;
+      document.getElementById("player2_score").innerHTML = player2_score;
+    }
+  }
+  if(question_turn == "player1")
+  {
+    question_turn = "player2"
+    document.getElementById("player_question").innerHTML = "Turno de Pergunta - " + player2_name;
+  }
+  else
+  {
+    question_turn = "player1"
+    document.getElementById("player_question").innerHTML = "Turno de Pergunta - " + player1_name;
+  }
+
+
+  if(answer_turn == "player1")
+  {
+    answer_turn = "player2"
+    document.getElementById("player_answer").innerHTML = "Turno de Resposta - " + player2_name
+  }
+  else
+  {
+    answer_turn = "player1"
+    document.getElementById("player_answer").innerHTML = "Turno de Resposta - " + player1_name
+  }
+}
